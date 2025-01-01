0ab56e6e5e2acd84bf8576cf27786a564b96e856
Emily Huang <144239497+EmilyHuang111@users.noreply.github.com>
2025-01-01 13:49:57 -0500

    Update 23.py

--- 23.py
+++ 23.py
@@ -22,26 +22,3 @@ def stop_signal(kit, channel):
 
 stop_signal(kit, 3)
 
-import time
-from adafruit_servokit import ServoKit
-
-kit = ServoKit(channels=16)
-
-kit.servo[2].angle = 180
-kit.continuous_servo[3].throttle = 1
-time.sleep(1)
-
-kit.continuous_servo[3].throttle = -1
-time.sleep(1)
-
-kit.servo[2].angle = 0
-kit.continuous_servo[3].throttle = 0
-time.sleep(1)
-
-kit.servo[2].angle = None
-
-def stop_signal(kit, channel):
-    pwm = kit._pca
-    pwm.channels[channel].duty_cycle = 0 
-
-stop_signal(kit, 3)
