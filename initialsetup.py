ff6d89b3666cde8bd7366f5f1d04e7828e14e55f
Emily Huang <144239497+EmilyHuang111@users.noreply.github.com>
2025-01-01 20:50:11 -0500

    Create initialsetup.py

---
+++ initialsetup.py
@@ -0,0 +1,33 @@
+import time
+from adafruit_servokit import ServoKit
+
+# Initialize ServoKit with 16 channels
+kit = ServoKit(channels=16)
+
+# Define servo channels
+LEG1F_CHANNEL = 0  # Front Left Forward
+LEG1B_CHANNEL = 1  # Front Left Backward
+LEG2F_CHANNEL = 8  # Front Right Forward
+LEG2B_CHANNEL = 9  # Front Right Backward
+LEG3F_CHANNEL = 2  # Back Left Forward
+LEG3B_CHANNEL = 3  # Back Left Backward
+LEG4F_CHANNEL = 10 # Back Right Forward
+LEG4B_CHANNEL = 11 # Back Right Backward
+
+def setup():
+    """Initialize default servo positions."""
+    set_all_servos(180)
+
+def set_all_servos(angle):
+    """Set all servos to the same angle."""
+    kit.servo[LEG1F_CHANNEL].angle = angle
+    kit.servo[LEG1B_CHANNEL].angle = angle
+    kit.servo[LEG2F_CHANNEL].angle = angle
+    kit.servo[LEG2B_CHANNEL].angle = angle
+    kit.servo[LEG3F_CHANNEL].angle = angle
+    kit.servo[LEG3B_CHANNEL].angle = angle
+    kit.servo[LEG4F_CHANNEL].angle = angle
+    kit.servo[LEG4B_CHANNEL].angle = angle
+
+if __name__ == "__main__":
+    setup()
